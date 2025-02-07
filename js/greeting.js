50d31b2d039a5e28011c6bf8a848a7b745661cf1
DESKTOP-DE0VMBV\sangu <sangugi01@gmail.com>
2025-02-07 20:43:21 +0900

    first_commit

---
+++ js/greeting.js
@@ -0,0 +1,28 @@
+const loginForm = document.querySelector(".login-form");
+const greeting = document.querySelector("#greeting")
+const loginInput = loginForm.querySelector("input");
+
+const HIDDEN_CLASSNAME = "hidden"
+const USERNAME_KEY = "username"
+
+const onLoginSubmit = (e) => {
+	e.preventDefault();
+	const username = loginInput.value;
+	console.log(username);
+	loginForm.classList.toggle(HIDDEN_CLASSNAME);
+	localStorage.setItem(USERNAME_KEY, username);
+	greeting.innerText = `hello ${username}!`
+	greeting.classList.toggle(HIDDEN_CLASSNAME);
+}
+
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if (savedUsername === null) {
+	loginForm.classList.remove(HIDDEN_CLASSNAME);
+} else {
+	loginForm.classList.add(HIDDEN_CLASSNAME);
+	greeting.innerText = `hello ${savedUsername}!`
+	greeting.classList.remove(HIDDEN_CLASSNAME);
+}
+
+loginForm.addEventListener("submit", onLoginSubmit);
